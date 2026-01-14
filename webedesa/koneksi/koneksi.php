086a95d8ab2572e8f4f7146068e7183162e138fc
hatakezon <raflydwinugroho9@gmail.com>
2026-01-14 12:01:50 +0700

    Update koneksi.php

--- webedesa/koneksi/koneksi.php
+++ webedesa/koneksi/koneksi.php
@@ -1,12 +1,21 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = ""; // Kosongkan jika pakai XAMPP default
-$db   = "dbdesa";
+// Mengambil data dari Environment Variables Vercel
+$host = getenv('DB_HOST');
+$user = getenv('DB_USER');
+$pass = getenv('DB_PASS');
+$db   = getenv('DB_NAME');
+
+// Jika variabel kosong (artinya sedang dijalankan di laptop/XAMPP), pakai settingan lokal
+if(!$host){
+    $host = 'localhost';
+    $user = 'root';
+    $pass = '';
+    $db   = 'db_desa_baru';
+}
 
 $koneksi = mysqli_connect($host, $user, $pass, $db);
 
-if (!$koneksi) {
-    die("Koneksi Gagal: " . mysqli_connect_error());
+if(!$koneksi){
+    die("Gagal Terhubung: " . mysqli_connect_error());
 }
 ?>
